a0d1a1ba8ca0d6e96f818e42fd59bac6955e3a91
Cedric <cedric.du.50@gmail.com>
2023-10-23 00:18:42 +0200

    création ressource item, architecture GameViewjf:w

---
+++ src/components/RessourceItem.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="d-flex">
+        <div class="d-flex flex-row">
+            <p class="align-middle">{{ ressource_name }}</p>
+            <p class="align-middle">{{ ressource_value }}</p>
+        </div>
+        <input/>
+        <div class="d-flex flex-column">
+            <button >+</button>
+            <button >-</button>
+        </div>
+        <button>set</button>
+        <button>reset</button>
+        <button>delete</button>
+    </div> 
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            ressource_name:"Pv",
+            ressource_value:"145/145"
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,9 +1,32 @@
-<script>
-
+<script setup>
+import H2Component from '../components/H2Component.vue'
+import RessourceItem from '../components/RessourceItem.vue'
 </script>
-<template>
-    <div></div>
-</template>
-<style>
 
+<template>
+    <div class="d-flex flex-column m-auto">
+        <H2Component :title="title"/>
+        <div>
+            <RessourceItem/>
+        </div>    
+        <div class="d-flex flex-column">
+            <img class="m-auto" src="../assets/plus.svg" alt="plus" id="icon-plus"/>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            title : "Test"
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    #icon-plus{
+        width:6em
+    }
 </style>
